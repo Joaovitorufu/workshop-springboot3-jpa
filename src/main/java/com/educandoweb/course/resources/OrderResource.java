4562fc01fd62822f7f7ea230900e77e638512cf2
Joaovitorufu <joaovitorufu@gmail.com>
2024-08-13 21:20:45 -0300

    Order, instant, ISO 8601 corrected

--- src/main/java/com/educandoweb/course/resources/OrderResource.java
+++ src/main/java/com/educandoweb/course/resources/OrderResource.java
@@ -2,6 +2,7 @@ package com.educandoweb.course.resources;
 
 import com.educandoweb.course.entities.Order;
 import com.educandoweb.course.services.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +15,7 @@ import java.util.List;
 @RequestMapping(value = "/orders")
 public class OrderResource {
 
+    @Autowired
     private OrderService orderService;
 
     @GetMapping
